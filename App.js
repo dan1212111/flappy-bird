1dfda0e4fd22ce00b8ea40e5cd7c9647ed58c7f0
Daniel Mc <danielmc122333@icloud.com>
2022-04-10 14:16:50 +0100

    completed core project

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
-
-
 import React from "react"
 import { useState, useEffect } from "react"
-import { StyleSheet, Text, View, Dimensions } from "react-native"
+import { StyleSheet, Text, View, Dimensions, TouchableWithoutFeedback } from "react-native"
 import Bird from "./component/Bird"
 import Obstacles from "./component/Obstacles"
 
@@ -12,22 +10,14 @@ export default function App() {
   /* setting bird in middle of screen + timer */
   const birdLeftSide = screenWidth / 2
   const [birdBottom, setBirdBottom] = useState(screenHeight / 2)
+  const [gameOver, setGameOver] = useState(false)
+  const [score, setScore] = useState(0)
   const gravity = 3
   let timer
-  /* obstacles state */
-  const [obstacles, setObstacles] = useState(screenWidth)
-  const [obstaclesTwo, setObstaclesTwo] = useState(screenWidth + screenWidth/2 + 30)
-  const [obstaclesHeightNeg, setObstaclesHeightNeg] = useState(0)
-  const [obstaclesHeightNegTwo, setObstaclesHeightNegTwo] = useState(0)
-  const obstacleWidth = 60
-  const obstacleHeight = 300
-  const gap = 200
-  let obstacleLeftTimer
-  let obstacleLeftTimerTwo
 
   /* making bird fall */
   useEffect(() => {
-    if (birdBottom > 0) {
+    if (birdBottom > 0 && gameOver === false) {
       timer = setInterval(() => {
         setBirdBottom((birdBottom) => birdBottom - gravity)
       }, 30)
@@ -38,65 +28,24 @@ export default function App() {
     }
   }, [birdBottom])
 
-  /* adding first set of obstacles */
-  // ONE
-  useEffect(() => {
-    if (obstacles > -obstacleWidth) {
-      obstacleLeftTimer = setInterval(() => {
-        setObstacles((obstacles) => obstacles - 5)
-      }, 30)
-      return () => {
-        clearInterval(obstacleLeftTimer)
-      }
-    } else {
-      setObstacles(screenWidth)
-      setObstaclesHeightNeg( - Math.random() * 100)
-    }
-  }, [obstacles])
-
-  //TWO
-  useEffect(() => {
-    if (obstaclesTwo > -obstacleWidth) {
-      obstacleLeftTimerTwo = setInterval(() => {
-        setObstaclesTwo((obstaclesTwo) => obstaclesTwo - 5)
-      }, 30)
-      return () => {
-        clearInterval(obstacleLeftTimerTwo)
-      }
-    } else {
-      setObstaclesTwo(screenWidth)
-      setObstaclesHeightNegTwo( - Math.random() * 100)
-    }
-  }, [obstaclesTwo])
-
-//check for collisions
-
-
-
-
-
+/* making bird jump */
+function jump () {
+  if(gameOver === false && birdBottom < screenHeight) {
+    setBirdBottom(birdBottom => birdBottom + 50)
+  }
+}
 
 
 
 
   return (
+    <TouchableWithoutFeedback onPress={jump}> 
     <View style={styles.container}>
+      {gameOver === true && <Text>Score: {score}</Text>}
       <Bird birdLeftSide={birdLeftSide} birdBottom={birdBottom} />
-      <Obstacles
-        obstacles={obstacles}
-        obstacleWidth={obstacleWidth}
-        obstacleHeight={obstacleHeight}
-        obstaclesHeightNeg={obstaclesHeightNeg}
-        gap={gap}
-      />
-      <Obstacles
-        obstacles={obstaclesTwo}
-        obstacleWidth={obstacleWidth}
-        obstacleHeight={obstacleHeight}
-        obstaclesHeightNeg={obstaclesHeightNegTwo}
-        gap={gap}
-      />
+      <Obstacles screenWidth={screenWidth} screenHeight={screenHeight} birdBottom={birdBottom} setGameOver={setGameOver} setScore={setScore}/>
     </View>
+    </TouchableWithoutFeedback>
   )
 }
 
--- component/Obstacles.js
+++ component/Obstacles.js
@@ -1,8 +1,75 @@
 import React from "react"
 import { View } from "react-native"
+import { useState, useEffect } from "react"
 
 export default function Obstacles(props) {
-const {obstacles, obstacleWidth, obstacleHeight, gap, obstaclesHeightNeg} = props
+const { screenWidth, screenHeight, birdBottom, setGameOver, setScore } = props
+const [obstacles, setObstacles] = useState(screenWidth)
+const [obstaclesTwo, setObstaclesTwo] = useState(screenWidth + screenWidth/2 + 30)
+const [obstaclesHeightNeg, setObstaclesHeightNeg] = useState(0)
+const [obstaclesHeightNegTwo, setObstaclesHeightNegTwo] = useState(0)
+const obstacleWidth = 60
+const obstacleHeight = 300
+const gap = 200
+let obstacleLeftTimer
+let obstacleLeftTimerTwo
+
+  // ONE
+  useEffect(() => {
+    if (obstacles > -obstacleWidth) {
+      obstacleLeftTimer = setInterval(() => {
+        setObstacles((obstacles) => obstacles - 5)
+      }, 30)
+      return () => {
+        clearInterval(obstacleLeftTimer)
+      }
+    } else {
+      setObstacles(screenWidth)
+      setObstaclesHeightNeg( - Math.random() * 100)
+      setScore(score => score + 1)
+    }
+  }, [obstacles])
+
+  //TWO
+  useEffect(() => {
+    if (obstaclesTwo > -obstacleWidth) {
+      obstacleLeftTimerTwo = setInterval(() => {
+        setObstaclesTwo((obstaclesTwo) => obstaclesTwo - 5)
+      }, 30)
+      return () => {
+        clearInterval(obstacleLeftTimerTwo)
+      }
+    } else {
+      setObstaclesTwo(screenWidth)
+      setObstaclesHeightNegTwo( - Math.random() * 100)
+      setScore(score => score + 1)
+    }
+  }, [obstaclesTwo])
+
+  //check for collisions
+  useEffect(() => {
+    if (
+      ((birdBottom < (obstaclesHeightNeg + obstacleHeight + 30) ||
+      birdBottom > (obstaclesHeightNeg + obstacleHeight + gap -30)) &&
+      (obstacles > screenWidth/2 -30 && obstacles < screenWidth/2 + 30 )
+      )
+      || 
+      ((birdBottom < (obstaclesHeightNegTwo + obstacleHeight + 30) ||
+      birdBottom > (obstaclesHeightNegTwo + obstacleHeight + gap -30)) &&
+      (obstaclesTwo > screenWidth/2 -30 && obstaclesTwo < screenWidth/2 + 30 )
+      )
+      ) 
+      {
+      console.log('game over')
+      gameOver()
+    }
+  })
+
+const gameOver = () => {
+  setGameOver(true)
+  clearInterval(obstacleLeftTimer)
+  clearInterval(obstacleLeftTimerTwo) 
+}
 
 
   return (
@@ -24,9 +91,9 @@ const {obstacles, obstacleWidth, obstacleHeight, gap, obstaclesHeightNeg} = prop
           width: obstacleWidth,
           height: obstacleHeight,
           left: obstacles,
-          bottom:  obstaclesHeightNeg,
+          bottom:  obstaclesHeightNegTwo,
         }}
       />
     </>
   )
-}
+      }
